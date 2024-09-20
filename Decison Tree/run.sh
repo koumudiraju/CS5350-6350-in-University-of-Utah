8f678716623a542fabc5b1c93c565396156148ce
koumudiraju <72028502+koumudiraju@users.noreply.github.com>
2024-09-20 00:33:49 -0600

    Update run.sh

--- Decison Tree/run.sh
+++ Decison Tree/run.sh
@@ -1,1 +1,10 @@
+#!/bin/sh
 
+echo "Training and Test errors for Car DataSet"
+python3 Car_Decision_Tree.py
+
+echo "Training and Test errors for Bank DataSet -unknown as feature value"
+python3 Bank_Decision_Tree.py
+
+echo " Training and Test errors for Bank DataSet -unknown as missing value"
+python3 Unknown_Bank_Decision_Tree.py
